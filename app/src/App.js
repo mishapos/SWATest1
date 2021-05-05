eba45f10bd843799ca09088b14aab50087b3d8f6
Michail Shaposhnikov <mishapos@microsoft.com>
2021-05-04 22:07:35 -0700

    Init commit

---
+++ app/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let response = await fetch(`api/message`);
+  let message = await response.json();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Look at this really cool spinny thing we have here. Visible update.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Everything is fine. Just fine. Absolutely fine.
+          message: {message}
+        </a>
+        <a 
+          className="App-link"
+          href="/.auth/login/aad"
+        >
+        Log in with Azure AD
+        </a>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
