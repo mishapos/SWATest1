07e695bf77816e021c0f38be72f4aa784d6cd0e5
Michail Shaposhnikov <mishapos@microsoft.com>
2021-06-15 09:44:21 -0700

    Update index.js

--- app/src/index.js
+++ app/src/index.js
@@ -10,26 +10,27 @@ import axios from 'axios';
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 class FetchDemo extends React.Component {
-    state = {
-      names: []
-    }
-  
-    componentDidMount() {
-      axios.get(`/api/${this.props.subreddit}`)
-        .then(res => {
-          console.log(res.data[0])
-          var names = res.data;
-          this.setState({names});
-        });
-    }
-  
-    render() {
-      return (
-        <div className="App">
+  state = {
+    names: []
+  }
+
+  componentDidMount() {
+    axios.get(`/api/${this.props.subreddit}`)
+      .then(res => {
+        console.log(res.data[0])
+        var names = res.data;
+        this.setState({ names });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Look at this really cool spinny thing we have here!!!
+            Misha was here.
           </p>
           <a
             className="App-link"
@@ -39,23 +40,23 @@ class FetchDemo extends React.Component {
           >
             Everything is fine.
           </a>
-          <a 
+          <a
             className="App-link"
             href="https://victorious-coast-02855ed10.azurestaticapps.net/.auth/login/aad"
           >
-          Log in with Azure AD
+            Log in with Azure AD
           </a>
           <p>{this.state.names}</p>
         </header>
       </div>
-      );
-    }
+    );
   }
-  
-  ReactDOM.render(
-    <FetchDemo subreddit="getList"/>,
-    document.getElementById('root')
-  );
+}
+
+ReactDOM.render(
+  <FetchDemo subreddit="getList" />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
